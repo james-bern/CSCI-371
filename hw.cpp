699c466634bef4e504f4d4625633c903aeb7ff96
Jim <jamesmbern@gmail.com>
2022-11-01 13:48:46 -0400

    push.bat

--- hw.cpp
+++ hw.cpp
@@ -219,11 +219,12 @@ void hw8a() {
                             for (int j = 0; j < S; ++j) {
 
                                 double theta = renderer.angle_of_view;
+                                vec3 o = o_renderer;
                                 vec3 dir = V3(i - double(S) / 2, j - double(S) / 2, -(double(S) / 2) / tan(theta / 2)); // student answer from board
                                 if (hw8a_tweaks.draw_rays) {
                                     gl_color(monokai.red);
-                                    gl_vertex(o_renderer);
-                                    gl_vertex(o_renderer + dir);
+                                    gl_vertex(o);
+                                    gl_vertex(o + dir);
                                 }
 
                                 // TODO intersect with the scene
