622f969d5e769ae30bc750f82f95699aa2938009
james-bern <jmb15@williams.edu>
2023-02-01 04:02:16 -0500

    push.bat

--- main.cpp
+++ main.cpp
@@ -1,54 +1,35 @@
 #include "include.cpp"
 
-void hw00() {
-    while (cow_begin_frame()) {
-
-    }
-}
-
-int main() {
-    APPS {
-        APP(eg_kitchen_sink);
-        APP(hw00);
-    }
-    return 0;
-}
-
-#if 0
-// #define COW_NO_STYLE_GUIDE
-#include "include.cpp"
-
 void hw00a() {
-    vec2 s = {};
-
     Camera2D camera = { 3.0 };
-    while(cow_begin_frame()) {
+
+    vec3 color = V3(1.0, 0.0, 1.0);
+    vec2 vertex_positions[3] = {
+        V2(0.0, 0.0),
+        V2(1.0, 0.0),
+        V2(0.0, 1.0),
+    };
+
+    vec2 edge_1 = vertex_positions[1] - vertex_positions[0];
+    vec2 edge_2 = vertex_positions[2] - vertex_positions[0];
+    real triangle_area = .5 * ABS(cross(edge_1, edge_2));
+    printf("the triangle's area is %lf\n", triangle_area);
+
+
+
+    while (cow_begin_frame()) {
         camera_move(&camera);
         mat4 PV = camera_get_PV(&camera);
 
-        if (gui_button("randomize!")) {
-            s = { random_real(-1.0, 1.0), random_real(-1.0, 1.0) };
-        }
-
-        eso_begin(PV, SOUP_LINE_LOOP); {
-            eso_color(monokai.white);
-            eso_vertex(-1.0, -1.0);
-            eso_vertex(-1.0,  1.0);
-            eso_vertex( 1.0,  1.0);
-            eso_vertex( 1.0, -1.0);
-        } eso_end();
-
-        widget_drag(PV, 1, &s);
-        soup_draw(PV, SOUP_POINTS, 1, &s, NULL, monokai.purple);
+        soup_draw(PV, SOUP_TRIANGLES, 3, vertex_positions, NULL, color);
     }
 }
 
 int main() {
     APPS {
-        APP(eg_kitchen_sink);
         APP(hw00a);
     }
-
     return 0;
 }
-#endif
+
+
